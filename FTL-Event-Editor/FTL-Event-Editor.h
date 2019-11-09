43062e3e15b67d03f061940399c037e63b0bfe80
Xierumeng <xierumeng@hotmail.com>
2019-11-08 19:54:26 -0500

    NUKE

--- FTL-Event-Editor/FTL-Event-Editor.h
+++ FTL-Event-Editor/FTL-Event-Editor.h
@@ -2,11 +2,3 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
-
-#include "Linked_List.h"
-#include "Node.h"
-#include "Event.h"
-
-void textMenu(Linked_List<std::string>* textList);
-void eventMenu(Linked_List<Event>* eventList);
-void findEvent(Event* p_event) { //!!!!
