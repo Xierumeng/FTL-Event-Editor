44595f6663d68289d59a82dc7613c5a3400004db
Xierumeng <xierumeng@hotmail.com>
2019-11-10 00:40:03 -0500

    Accessing XML files

--- FTL-Event-Editor/FTL-Event-Editor.cpp
+++ FTL-Event-Editor/FTL-Event-Editor.cpp
@@ -2,7 +2,7 @@
 // ------------
 // Name:    FTL Event Editor
 // Goal:    To allow users to import, modify, create, test, and export events
-// Version: 0.1.4 //FROM PROGRAM_VERSION
+// Version: 0.1.4
 // For FTL: 1.6? The latest, anyway
 // Plan:    Adding own, simulating. Later: Import, export.
 // Author:  Xierumeng
@@ -27,14 +27,36 @@
 
 #include "FTL-Event-Editor.h"
 #include "tinyxml2.h"
-#include <QApplication>
-#include <QPushButton>
+#include <stdlib.h>
+//#include <QApplication>
+//#include <QPushButton>
 
 int main()
 {
-    tinyxml2::XMLDocument eventDoc;
-    eventDoc.LoadFile("D:/Games/Science Fiction and Space/FTL Faster Than Light/Modding/Dats/data/events.xml");
+    tinyxml2::XMLDocument eventXML;
+    eventXML.LoadFile("D:/FTLDats/data/events.xml");
 
+    ///*
+    // <FTL>
+    //   <event etc.>
+    //     <text> We are here
+    tinyxml2::XMLElement* textElement = eventXML.FirstChildElement("FTL")->FirstChildElement("event")->FirstChildElement("text");
+    // <text>Now we are here</text>
+    std::string text = textElement->GetText();
+    //*/
+    
+    ///*
+    // <FTL>
+    //   <event etc.>
+    //     <boarders etc.> We are here
+    tinyxml2::XMLElement* boarderElement = eventXML.FirstChildElement("FTL")->FirstChildElement("event")->FirstChildElement("boarders");
+    int minBoarders = 0;
+    // <boarders min="Now we are here">
+    boarderElement->QueryIntAttribute("min", &minBoarders);
+
+    std::cout << "Hello? " << text << " " << minBoarders << std::endl;
+    //eventXML.Print();
+    
     system("PAUSE");
     return 0;
 }
